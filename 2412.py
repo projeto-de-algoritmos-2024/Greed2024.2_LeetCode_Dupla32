bfae81c832cdac3b82c9b1ef92c120e7ea9bfa40
lucasdray <pedrolucasdourado@hotmail.com>
2025-01-20 11:18:01 -0300

    2412 wrong

--- 2412.py
+++ 2412.py
@@ -3,11 +3,14 @@ class Solution:
         # tem que ordenar de um jeito que atenda em qualquer ordem de transação logo a pior 
         # é deixar a que tem menos cashback primeiro e as que tem mais por ultimo
 
-        transactions.sort(key = lambda x: x[1])
+        transactions.sort(key = lambda x: x[0] - x[1])
         valor_necessario = 0
         tamanho = len(transactions)
 
         for transacao in transactions:
+            if transacao[0] == 0:
+                continue
             valor_necessario += transacao[0] - transacao[1]
+
         valor_necessario += ( transactions[tamanho - 1][1]) # o ultimo cashback n entra na soma
         return valor_necessario
