c49599ca40ab340497f6479b5745a35fe38603bf
lucasdray <pedrolucasdourado@hotmail.com>
2025-01-20 09:25:19 -0300

    1029 fail

--- 1029.py
+++ 1029.py
@@ -1,9 +1,15 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         soma = 0
+        costs.sort(key=lambda x: x[0] )
+        tamanho = len(costs)/ 2
+        tamanho_a = 0
+
         for n in costs:
-            if n[0] <= n[1]:
+            if n[0] <= n[1] or tamanho_a < tamanho:
+                tamanho_a += 1
                 soma += n[0]
             else:
                 soma += n[1]
+
         return soma
